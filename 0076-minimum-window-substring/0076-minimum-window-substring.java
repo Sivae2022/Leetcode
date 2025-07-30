de4c45861b037301a79fb39e3cb93e3353bac7ab
Siva E <123160024+Sivae2022@users.noreply.github.com>
2025-07-30 09:55:45 +0530

    Time: 21 ms (35.73%), Space: 45.8 MB (15.87%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,26 +1,47 @@
 class Solution {
-  public String minWindow(String s, String t) {
-    int[] count = new int[128];
-    int required = t.length();
-    int bestLeft = -1;
-    int minLength = s.length() + 1;
+    public String minWindow(String s, String t) {
+     
+        if (s == null || t == null || s.length() < t.length()) return "";
 
-    for (final char c : t.toCharArray())
-      ++count[c];
-
-    for (int l = 0, r = 0; r < s.length(); ++r) {
-      if (--count[s.charAt(r)] >= 0)
-        --required;
-      while (required == 0) {
-        if (r - l + 1 < minLength) {
-          bestLeft = l;
-          minLength = r - l + 1;
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
-        if (++count[s.charAt(l++)] > 0)
-          ++required;
-      }
+
+        int left = 0, right = 0, minLen = Integer.MAX_VALUE;
+        int start = 0;
+        int required = map.size();
+        int formed = 0;
+
+        Map<Character, Integer> windowCounts = new HashMap<>();
+
+        while (right < s.length()) {
+            char c = s.charAt(right);
+            windowCounts.put(c, windowCounts.getOrDefault(c, 0) + 1);
+
+            if (map.containsKey(c) && windowCounts.get(c).intValue() == map.get(c).intValue()) {
+                formed++;
+            }
+
+            while (left <= right && formed == required) {
+                if (right - left + 1 < minLen) {
+                    minLen = right - left + 1;
+                    start = left;
+                }
+
+                char lChar = s.charAt(left);
+                windowCounts.put(lChar, windowCounts.get(lChar) - 1);
+                if (map.containsKey(lChar) && windowCounts.get(lChar).intValue() < map.get(lChar).intValue()) {
+                    formed--;
+                }
+                left++;
+            }
+
+            right++;
+        }
+
+        return minLen == Integer.MAX_VALUE ? "" : s.substring(start, start + minLen);
     }
 
-    return bestLeft == -1 ? "" : s.substring(bestLeft, bestLeft + minLength);
-  }
+  
 }
